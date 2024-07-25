5303c301f306615291c98b54bcacf104bc817179
imonaar <kevinmonari22@gmail.com>
2024-07-25 11:51:03 +0300

    final

---
+++ sw.js
@@ -0,0 +1,60 @@
+// The version of the cache.
+const VERSION = "v1";
+
+// The name of the cache
+const CACHE_NAME = `period-tracker-${VERSION}`;
+
+// The static resources that the app needs to function.
+const APP_STATIC_RESOURCES = [
+    "/",
+    "/index.html",
+    "/app.js",
+    "/style.css",
+    "/icons/wheel.svg",
+];
+
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        (async () => {
+            const cache = await caches.open(CACHE_NAME);
+            cache.addAll(APP_STATIC_RESOURCES);
+        })(),
+    );
+});
+
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        (async () => {
+            const names = await caches.keys();
+            await Promise.all(
+                names.map((name) => {
+                    if (name !== CACHE_NAME) {
+                        return caches.delete(name);
+                    }
+                }),
+            );
+            await clients.claim();
+        })(),
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    if (event.request.mode === "navigate") {
+        event.respondWith(caches.match("/"));
+        return;
+    }
+
+    event.respondWith(
+        (async () => {
+            const cache = await caches.open(CACHE_NAME);
+            const cachedResponse = await cache.match(event.request.url);
+            if (cachedResponse) {
+
+                return cachedResponse;
+            }
+            return new Response(null, { status: 404 });
+        })(),
+    );
+});
